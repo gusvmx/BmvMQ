9d2e61de07229f1a920e0120015e50e411ee6145
Gustavo Vargas <gvargas@bursatec.com.mx>
2014-09-24 12:05:15 -0500

    Cambia de proveedor JMS de JBoss ActiveMQ a Soniq
    El cambio se da para proveer una version de bmv-mq que se conecta a
    Sonic. Esto es para facilitar la migración de Soniq a ActiveMQ donde
    algunos componentes comienzan a migrar a BmvMQ y posteriormente, cuando
    esten listos para cambiar de broker JMS, simplemente cambian la versión
    de bmv-mq

--- bmvMQ/src/main/java/com/bursatec/bmvmq/config/ApplicationConfiguration.java
+++ bmvMQ/src/main/java/com/bursatec/bmvmq/config/ApplicationConfiguration.java
@@ -4,14 +4,14 @@
 package com.bursatec.bmvmq.config;
 
 import javax.jms.ConnectionFactory;
+import javax.jms.JMSException;
 
-import org.apache.activemq.ActiveMQConnectionFactory;
-import org.apache.activemq.pool.PooledConnectionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.connection.CachingConnectionFactory;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.jms.support.destination.DynamicDestinationResolver;
 import org.springframework.jms.support.destination.JndiDestinationResolver;
@@ -35,20 +35,23 @@ public class ApplicationConfiguration {
 	
 	/**
 	 * @return Un pool de la fábrica de conexiones.
+	 * @throws JMSException Si no se puede crear un conectionFactory de Sonic.
 	 */
 	@Bean
-	ConnectionFactory jmsConnectionFactory() {
-		ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory();
-		connectionFactory.setBrokerURL(configuration.getUrl());
-		connectionFactory.setUserName(configuration.getUsername());
-		connectionFactory.setPassword(configuration.getPassword());
-		PooledConnectionFactory pooledConnectionFactory = new PooledConnectionFactory(connectionFactory);
-		pooledConnectionFactory.setMaxConnections(configuration.getMaxConnections());
+	ConnectionFactory jmsConnectionFactory() throws JMSException {
+		progress.message.jclient.ConnectionFactory connectionFactory = new progress.message.jclient.ConnectionFactory();
+		connectionFactory.setConnectionURLs(configuration.getUrl());
+		connectionFactory.setFaultTolerant(true);
+		connectionFactory.setFaultTolerantReconnectTimeout(configuration.getReconnectionInterval());
+		connectionFactory.setDefaultUser(configuration.getUsername());
+		connectionFactory.setDefaultPassword(configuration.getPassword());
+		CachingConnectionFactory cachingConnectionFactory = new CachingConnectionFactory(connectionFactory);
+		cachingConnectionFactory.setSessionCacheSize(configuration.getMaxConnections());
 		
-		logger.info("Pooled connection factory has been initialised with the following values. "
+		logger.info("Caching connection factory has been initialised with the following values. "
 				+ "URL:{}, Username:{}, MaxConnections:{}", 
 				configuration.getUrl(), configuration.getUsername(), configuration.getMaxConnections());
-		return pooledConnectionFactory;
+		return cachingConnectionFactory;
 	}
 	
 	/**
